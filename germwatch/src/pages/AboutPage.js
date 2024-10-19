16800875d49b51ee96ba85beb20a0e29fbeb8c72
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2024-10-19 13:53:22 -0400

    Print

--- germwatch/src/pages/AboutPage.js
+++ germwatch/src/pages/AboutPage.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-function AboutPage() {
+const AboutPage = () => {
   return (
     <div className="App">
-
+    <h1>About Us</h1>
+    <p>Welcome to GermWatch! We are a team of four highly motivated computer science students with a drive for health safety.</p>
+    <p>Upon arriving at college, we quickly noticed how fast illness was spread across campus. This led us to question if there was a way to check for illness and disease in our area.</p>
+    <p>This curiosity is what sparked GermWatch, a webstite application where you can enter your city an index of sicknesses and diseases will be shown.                                                                                       fghjjjhfgjhhjhjjghfjghfhffgjhfgjhfgjhfgjfhgjhfgjfhgfjhgjfhgjfhjjghfghjfgjfhgjhfjjfghfghjgjfhvfjghvvfgjhvjfghjgfhjfghjfghjfgfjjfghjfghfgjhjgjgfhjfghjfghfgjhfgjhgjfhfgjhvfgjhvvjfghfghjvffgjghfgfjghfjfgjjgfhgfhgfgfhjhgfjghfjgfhjgfhjghfj
+    <p>Jack Bialecki - Computer Science Major at the University of Cincinnati.</p>
+    <p>Dana Costa III - Computer Science and Engineering Major at The Ohio State University.</p>
+    <p>Alex Guller - Computer Science and Engineering Major at The Ohio State University.</p>
+    <p>Shrey Kumar - Computer Science and Engineering Major at The Ohio State University.</p>
+    </p>
     </div>
   );
-}
+};
 
 export default AboutPage;
