0c7eb5616eb93c24e8170c13d2356fbaa18140e6
Jack Bialecki <Jrbialecki@icloud.com>
2024-10-19 12:03:14 -0400

    updated

--- germwatch/src/App.js
+++ germwatch/src/App.js
@@ -12,36 +12,6 @@ function App() {
       <Routes />
       <Footer />
     </Router>
-=======
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import About from 'About.js';
-import Map from 'Map.js'
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <b>GermWatch</b>
-        </p>
-          <input type="text" placeholder="Enter City:" />
-          <Router>
-          < >
-            <Navigation />
-            <UpButton />
-            <Switch>
-              <Route path=/>
-            </Switch>
-          </>
-        </Router>
-          <button>-{'>'}</button>
-      </header>
-    </div>
->>>>>>> Stashed changes
   );
 }
 
