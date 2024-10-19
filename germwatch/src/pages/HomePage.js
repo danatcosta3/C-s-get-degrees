02f7bf6978d85566278dc23f37d50c89e3e33170
AlexGuller <alex.1guller@gmail.com>
2024-10-19 16:10:59 -0400

    Revert "Homepage update"
    This reverts commit 17c661f20834db775223e2bf36025778c01895b9.

--- germwatch/src/pages/HomePage.js
+++ germwatch/src/pages/HomePage.js
@@ -18,36 +18,28 @@ function HomePage() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="suggest-container">
-            <ul>
-                <li>City</li>
-                <li>City</li>
-                <li>City</li>
-                <li>City</li>
-                <li>City</li>
-                <li>City</li>
-                <li>City</li>
-                <li>City</li>
-            </ul>
-        </div>
-        <div className="logo-container">
-          <img src={"GermWatchLogo2.png"} alt="GermWatch Logo" className="App-logo" />
-          <h1 className="germwatch-title">GermWatch</h1>
-        </div>
-        <div className="search-container">
-          <input 
+        <p>
+          <b>
+            <center>
+              <h1 className="germwatch-title">GermWatch</h1>
+            </center>
+          </b>
+          <center>
+            <img src={"GermWatchLogo2.png"} alt="GermWatch Logo" className="App-logo" />
+          </center>
+        </p>
+        <center>
+          <input
             type="text"
             placeholder="Enter City:"
             value={city} // Set the input value from state
             onChange={handleInputChange} // Handle input change
             className={city === "" ? "input-dim" : "input-normal"} // Conditional class for styling
           />
-        
-          <button className="button" onClick={handleButtonClick} disabled={city === ""}>
-            {"->"}
-          </button>
-        </div>
-        
+          <button onClick={handleButtonClick} disabled={city === ""}>
+            {">"}
+          </button>{" "}
+        </center>
       </header>
     </div>
   );
