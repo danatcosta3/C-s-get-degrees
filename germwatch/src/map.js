4c9d39227372eb1fd7ab05c7fa54ca3c4e8a44cb
Dana Costa <danatcosta3@yahoo.com>
2024-10-19 11:36:39 -0400

    added map

---
+++ germwatch/src/map.js
@@ -0,0 +1,13 @@
+// src/About.js
+import React from "react";
+
+const map = () => {
+  return (
+    <div>
+      <h2>map</h2>
+      <p>This page is a map.</p>
+    </div>
+  );
+};
+
+export default map;
