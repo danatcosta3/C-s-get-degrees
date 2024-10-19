204733acedc408d4f2f38d605557357ae41a6eef
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2024-10-19 17:29:52 -0400

    Revert "about page fixed"
    This reverts commit 63f1a5c0d9c83de97d766ae2f9809cdf06b7c776.

--- germwatch/src/pages/AboutPage.js
+++ germwatch/src/pages/AboutPage.js
@@ -4,22 +4,11 @@ import './AboutPage.css';
 const AboutPage = () => {
   return (
     <div className="App">
-   <h2 className="about-title">Stay Informed About Health Trends in Your Area</h2>
-      <p className="about-description">
-        GermWatch is your go-to tool for staying updated on potential health risks such as diseases, illnesses, and other health concerns in your area.
-      </p>
-
-        <h3 class="about-textHeader">Our Mission</h3>
-        <p className="about-text">
-          We are a team of four passionate computer science students, driven by our commitment to promoting health and safety. Upon noticing how rapidly illnesses can spread across campuses and communities, we set out to build GermWatch as a way to raise awareness and help people stay informed.
-        </p>
- 
-     
-        <h3 class="about-textHeader">How It Works</h3>
-        <p className="about-text">
-          Simply enter your location, and GermWatch will provide a comprehensive index of local health concerns. You can click on each concern to explore detailed information and advice on how to stay safe.
-        </p>
-     
+    <h1 className="about-title">About</h1>
+    <h2>Be Aware of Health Trends in your Area</h2>
+    <p>GermWatch will notify you of health risk such as diseases, illnesses, and std's that are presesnt your area. </p>
+    <p>We are a team of four highly motivated computer science students with a commitment to promoting health and safety in our communities.</p>
+    <p>After you enter your location, a index of health concerns in your area will be presesnted with the descriptions of each also available.</p>
     </div>
   );
 };
