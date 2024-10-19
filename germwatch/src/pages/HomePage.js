5a6400d764816a57f925654ab4dbda52347745b0
Jack Bialecki <Jrbialecki@icloud.com>
2024-10-19 12:04:14 -0400

    Update hpme

--- germwatch/src/pages/HomePage.js
+++ germwatch/src/pages/HomePage.js
@@ -3,25 +3,27 @@ import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Map from 'Map.js'
 
 function HomePage() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <b>GermWatch</b>
-          </p>
-            <input type="text" placeholder="Enter City:" />
-            <Router>
-            < >
-              <Navigation />
-              <UpButton />
-              <Switch>
-                <Route path="/Map" component={Map}/>
-              </Switch>
-            </>
-          </Router>
-            <button>-{'>'}</button>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <b>GermWatch</b>
+        </p>
+          <input type="text" placeholder="Enter City:" />
+          <Router>
+          < >
+            <Navigation />
+            <UpButton />
+            <Switch>
+              <Route path="/Map" component={Map}/>
+            </Switch>
+          </>
+        </Router>
+          <button>-{'>'}</button>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
