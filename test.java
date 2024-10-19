80a8b83898db00d5faf7b951dc3d9e9c284bea4f
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2024-10-19 09:51:15 -0400

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,15 @@
-import java.io.*;
-import java.util.*;
+ 
 
 public class test {
 
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++)
-        {
-            
+        for(int i = 0; i < 10; i++){
             System.out.print(i+1 + " ");
         }
+        printName("shrey");
+    }
+
+    public static void printName(String name){
+        System.out.print("Your name is " + name);
     }
 }
