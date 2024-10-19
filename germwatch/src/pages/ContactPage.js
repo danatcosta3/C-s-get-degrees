dd7257bba32080a9fddd9ebe7bde1bb2037c3007
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2024-10-19 16:54:15 -0400

    contact

--- germwatch/src/pages/ContactPage.js
+++ germwatch/src/pages/ContactPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-function ContactPage() {
+const ContactPage = () => {
   return (
     <div className="App">
-
+    <h2>Conact Us Here </h2>
+    <p>Jackson Bialecki - <a href="mailto:bialecjr@mail.uc.edu">bialecjr@mail.uc.edu</a></p>
+    <p>Dana Costa III - <a href="mailto:costa.182@buckeyemail.osu.edu">costa182@buckeyemail.osu.edu</a></p>
+    <p>Alex Guller - <a href="mailto:guller.4@buckeyemail.osu.edu">guller.4@buckeyemail.osu.edu</a></p>
+    <p>Shrey Kumar - <a href="kumar.1166@buckeyemail.osu.edu">kumar.1166@osu.edu</a></p>
     </div>
   );
 }
