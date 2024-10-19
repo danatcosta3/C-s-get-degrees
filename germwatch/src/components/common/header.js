628d2ff8f072a02d794ac7a2fcb21b1ef9cc3037
Jack Bialecki <Jrbialecki@icloud.com>
2024-10-19 13:46:12 -0400

    Header css

--- germwatch/src/components/common/header.js
+++ germwatch/src/components/common/header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './header.css';
 
 function Header() {
   return (
     <header>
       <nav>
-        <Link to="/" className="logo">HealthMap</Link>
+        <Link to="/" className="logo">GermWatch</Link>
         <ul>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/contact">Contact</Link></li>
