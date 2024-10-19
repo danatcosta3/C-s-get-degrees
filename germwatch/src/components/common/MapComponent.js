00464bbe6aaec751f47f0e4f2663ad41a188f52b
Dana Costa <danatcosta3@yahoo.com>
2024-10-19 12:54:25 -0400

    Added MapComponent

---
+++ germwatch/src/components/common/MapComponent.js
@@ -0,0 +1,40 @@
+// src/components/MapComponent.js
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Fixing the default icon issue with Leaflet
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+});
+
+const MapComponent = () => {
+  // Set initial position and zoom level
+  const position = [51.505, -0.09]; // Example coordinates for London
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{ height: "400px", width: "100%" }}
+    >
+      {/* Tile Layer from OpenStreetMap */}
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {/* Marker Example */}
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default MapComponent;
--- germwatch/src/pages/MapPage.js
+++ germwatch/src/pages/MapPage.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MapComponent from "../components/common/MapComponent";
 function MapPage() {
   return (
-    <div className="App">
-
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>This is the Map Page</h1>
+      <MapComponent /> {/* Render the MapComponent here */}
     </div>
   );
 }
