e3ea2e4efa85a4795ea51907cb569acfe0986dd4
AlexGuller <alex.1guller@gmail.com>
2024-10-19 12:01:52 -0400

    fifth

--- germwatch/src/App.js
+++ germwatch/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
@@ -11,6 +12,36 @@ function App() {
       <Routes />
       <Footer />
     </Router>
+=======
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import About from 'About.js';
+import Map from 'Map.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <b>GermWatch</b>
+        </p>
+          <input type="text" placeholder="Enter City:" />
+          <Router>
+          < >
+            <Navigation />
+            <UpButton />
+            <Switch>
+              <Route path=/>
+            </Switch>
+          </>
+        </Router>
+          <button>-{'>'}</button>
+      </header>
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
---
+++ germwatch/src/pages/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Map from 'Map.js'
+
+function HomePage() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            <b>GermWatch</b>
+          </p>
+            <input type="text" placeholder="Enter City:" />
+            <Router>
+            < >
+              <Navigation />
+              <UpButton />
+              <Switch>
+                <Route path="/Map" component={Map}/>
+              </Switch>
+            </>
+          </Router>
+            <button>-{'>'}</button>
+        </header>
+      </div>
+    );
+  }
