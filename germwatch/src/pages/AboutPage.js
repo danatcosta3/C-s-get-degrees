50bb14ddc2f0cbde2f56fe9c96e1599e67d7d61b
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2024-10-19 14:31:51 -0400

    seventh

--- germwatch/src/pages/AboutPage.js
+++ germwatch/src/pages/AboutPage.js
@@ -1,17 +1,14 @@
 import React from 'react';
+import './AboutPage.css';
 
 const AboutPage = () => {
   return (
     <div className="App">
-    <h1>About Us</h1>
-    <p>Welcome to GermWatch! We are a team of four highly motivated computer science students with a drive for health safety.</p>
-    <p>Upon arriving at college, we quickly noticed how fast illness was spread across campus. This led us to question if there was a way to check for illness and disease in our area.</p>
-    <p>This curiosity is what sparked GermWatch, a webstite application where you can enter your city an index of sicknesses and diseases will be shown.                                                                                       
-    <p>Jack Bialecki - Computer Science Major at the University of Cincinnati.</p>
-    <p>Dana Costa III - Computer Science and Engineering Major at The Ohio State University.</p>
-    <p>Alex Guller - Computer Science and Engineering Major at The Ohio State University.</p>
-    <p>Shrey Kumar - Computer Science and Engineering Major at The Ohio State University.</p>
-    </p>
+    <h1 className="about-title">About</h1>
+    <h2>Be Aware of Health Trends in your Area</h2>
+    <p>GermWatch will notify you of health risk such as diseases, illnesses, and std's that are presesnt your area. </p>
+    <p>We are a team of four highly motivated computer science students with a commitment to promoting health and safety in our communities.</p>
+    <p>After you enter your location, a index of health concerns in your area will be presesnted with the descriptions of each also available.</p>
     </div>
   );
 };
