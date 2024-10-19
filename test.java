f8783137c9b374ebb30ea17f3ffce2f6e6d2deaf
shreykumar <156003188+shreykumar12@users.noreply.github.com>
2024-10-19 09:53:11 -0400

    removed name

--- test.java
+++ test.java
@@ -6,10 +6,8 @@ public class test {
         for(int i = 0; i < 10; i++){
             System.out.print(i+1 + " ");
         }
-        printName("shrey");
+        
     }
 
-    public static void printName(String name){
-        System.out.print("Your name is " + name);
-    }
+    
 }
