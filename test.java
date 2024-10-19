7bdf2cda360ee67b2e46050e9c27aa8f12f9a4d1
Dana Costa <danatcosta3@yahoo.com>
2024-10-19 09:52:29 -0400

    test

--- test.java
+++ test.java
@@ -6,8 +6,10 @@ public class test {
     public static void main(String[] args) {
         for(int i = 0; i < 10; i++)
         {
-            
             System.out.print(i+1 + " ");
+            
         }
+    
     }
+
 }
