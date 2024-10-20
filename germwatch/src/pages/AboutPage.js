a628f7b421319a8cf84dff501aa5a142a7075018
Jack Bialecki <Jrbialecki@icloud.com>
2024-10-19 23:24:42 -0400

    Fixed about and contact pages

--- germwatch/src/pages/AboutPage.js
+++ germwatch/src/pages/AboutPage.js
@@ -3,23 +3,21 @@ import './AboutPage.css';
 
 const AboutPage = () => {
   return (
-    <div className="App">
-   <h2 className="about-title">Stay Informed About Health Trends in Your Area</h2>
+    <div className="about">
+      <h2 className="about-title">Stay Informed About Health Trends in Your Area</h2>
       <p className="about-description">
         GermWatch is your go-to tool for staying updated on potential health risks such as diseases, illnesses, and other health concerns in your area.
       </p>
 
-        <h3 class="about-textHeader">Our Mission</h3>
-        <p className="about-text">
-          We are a team of four passionate computer science students, driven by our commitment to promoting health and safety. Upon noticing how rapidly illnesses can spread across campuses and communities, we set out to build GermWatch as a way to raise awareness and help people stay informed.
-        </p>
- 
-     
-        <h3 class="about-textHeader">How It Works</h3>
-        <p className="about-text">
-        Simply enter your location, and GermWatch will provide a comprehensive index of local health concerns. You can select different health concerns
-        and diseases to change what you see on the map.</p>
-     
+      <h3 className="about-textHeader">Our Mission</h3>
+      <p className="about-text">
+        We are a team of four passionate computer science students, driven by our commitment to promoting health and safety. Upon noticing how rapidly illnesses can spread across campuses and communities, we set out to build GermWatch as a way to raise awareness and help people stay informed.
+      </p>
+
+      <h3 className="about-textHeader">How It Works</h3>
+      <p className="about-text">
+        Simply enter your location, and GermWatch will provide a comprehensive index of local health concerns. You can select different health concerns and diseases to change what you see on the map.
+      </p>
     </div>
   );
 };
--- germwatch/src/pages/ContactPage.js
+++ germwatch/src/pages/ContactPage.js
@@ -3,17 +3,19 @@ import './ContactPage.css';
 
 const ContactPage = () => {
   return (
-    <div className="App">
-    <h2 class="title">Contact Us Here </h2>
-    <img class="icon"src="134146_mail_email_icon.png" alt="email icon" />
-    <p class="main">Jackson Bialecki</p> 
-    <p class="emailAdjust"><a href="mailto:bialecjr@mail.uc.edu">bialecjr@mail.uc.edu</a></p>
-    <p class="main">Dana Costa III </p>
-    <p class="emailAdjust"><a href="mailto:costa.182@buckeyemail.osu.edu">costa182@buckeyemail.osu.edu</a></p>
-    <p class="main">Alex Guller</p>
-    <p class="emailAdjust"><a href="mailto:guller.4@buckeyemail.osu.edu">guller.4@buckeyemail.osu.edu</a></p>
-    <p class="main">Shrey Kumar</p>
-    <p class="emailAdjust"><a href="kumar.1166@buckeyemail.osu.edu">kumar.1166@buckeyemail.osu.edu</a></p>
+    <div>
+      <h2 class="title">Contact Us Here </h2>
+      <div class="image-container">
+      <img class="icon"src="134146_mail_email_icon.png" alt="email icon" />
+      </div>
+      <p class="main">Jackson Bialecki</p> 
+      <p class="emailAdjust"><a href="mailto:bialecjr@mail.uc.edu">bialecjr@mail.uc.edu</a></p>
+      <p class="main">Dana Costa III </p>
+      <p class="emailAdjust"><a href="mailto:costa.182@buckeyemail.osu.edu">costa182@buckeyemail.osu.edu</a></p>
+      <p class="main">Alex Guller</p>
+      <p class="emailAdjust"><a href="mailto:guller.4@buckeyemail.osu.edu">guller.4@buckeyemail.osu.edu</a></p>
+      <p class="main">Shrey Kumar</p>
+      <p class="emailAdjust"><a href="kumar.1166@buckeyemail.osu.edu">kumar.1166@buckeyemail.osu.edu</a></p>
     </div>
   );
 };
